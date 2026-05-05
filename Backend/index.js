c23c174fbda9c3897d4492dbe6ab44ae9c3add08
Ashutosh Shukla <theashutoshshukl@gmail.com>
2026-05-05 20:33:05 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: [
-            "https://ashutosh.pro/baatcheet"
+            "https://ashutosh.pro/baatcheet/"
         ],
         methods: ["GET", "POST"],
     },
