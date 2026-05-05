989191d6fa15b110564af05dfd3bd2778d216fec
shivoham01 <theashutoshshukl@gmail.com>
2026-05-05 17:17:09 +0530

    Deployement Error Fix

--- Frontend/src/socket.io/socket.js
+++ Frontend/src/socket.io/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://baatcheet-2xgu.onrender.com", {
   autoConnect: false,
 });
