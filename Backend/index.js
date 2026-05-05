1bad803cccb3ba8f4d85ba53a0aa75aecf1ccfcd
shivoham01 <theashutoshshukl@gmail.com>
2026-05-05 18:07:08 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,6 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: [
-            "http://localhost:5173",
             "https://shivoham01.github.io"
         ],
         methods: ["GET", "POST"],
