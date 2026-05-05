510c0a15119308254f81ed8eccd7a0ffabfb5daf
shivoham01 <theashutoshshukl@gmail.com>
2026-05-05 18:12:17 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: [
-            "https:/ashutosh.pro"
+            "https://ashutosh.pro/baatcheet"
         ],
         methods: ["GET", "POST"],
     },
